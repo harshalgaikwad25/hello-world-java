876988c6f0608fe4bbbfb9e51327c256e6da0fd0
Harshal Gaikwad <harshalgaikwad234@gmail.com>
2023-01-09 12:59:02 +0000

    auto trigger

--- HelloWorld.java
+++ HelloWorld.java
@@ -38,5 +38,6 @@ public class HelloWorld {
     System.out.println("Hello, World");
     System.out.println("Hello, World ");
     System.out.println("Hello, World ");
+    System.out.println("Hello, World this is auto triggering ");
   }
 }
